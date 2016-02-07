fa5895c7ff34c5e64e5f7b1cd037165a6a086c4a
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-02-07 22:29:44 +0500

    shabang, chmod, support of arg with '/'

--- download.py
+++ download.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python
+
 # coding: utf-8
 """
-Выкачивает все трэки со стены группы вконтакте в папку с её ID.
+Download all tracks from vk.com group wall to directory named by group id
 
-Задать группу можно аргументом:
-    python download.py <group_id>
-либо переменной окружения ID:
-    export ID=<group_id>
+How to set group id:
+    ./download.py <group_id>
+example:
+    ./download.py 18312682
+or by export environment variable VK_GROUP_ID:
+    export VK_GROUP_ID=<group_id>
+    ./download.py
+example
+    export VK_GROUP_ID=18312682
+    ./download.py
 
-По умолчанию выбран http://vk.com/newbeatmusic
+By default group_id set to http://vk.com/newbeatmusic
 
-ID должен быть в виде цифры, узнать можно скопировав ссылку на аватар группы.
+You can known group id by viewing URL of some post on its wall.
 """
 
 import os
@@ -22,69 +30,60 @@ from HTMLParser import HTMLParser
 
 HTML = HTMLParser()
 API_URL = 'https://api.vk.com/method'
-
-
-def get_group_id():
-    """сперва проверяем аргументы, затем env, затем берём дефолтное"""
-    return len(argv) > 1 and argv[1] or (os.environ.get('ID') or '18312682')
-
-
-def wall_post_count_get(url):
-    """узнаём число постов на стене"""
-    return json.load(urlopen(url + '&count=1')).get('response')[0]
+VK_GROUP_ID = os.environ.get('VK_GROUP_ID', '18312682')
 
 
 def simplify(attach):
-    """отдаём упрощённое описание трэка"""
+    """reduce dict and normalize url/title/performer values"""
     audio = attach.get('audio')
     url = audio.get('url')
     return {
         'url': url.replace('?' + urlparse(url).query, ''),
-        'title': HTML.unescape(audio['title']),
-        'performer': HTML.unescape(audio['performer']),
+        'title': HTML.unescape(audio.get('title', 'Unknown song')),
+        'performer': HTML.unescape(audio.get('performer', 'Unknown artist')),
     }
 
 
 def download(track, group_id):
-    """скачиваем и сохраняем трэк"""
+    """download and save tracks, skip if file already exist"""
     data = (track['performer'], track['title'])
-    print ("Download: %s - %s" % data).encode('utf-8'), track['url']
-    # в два захода, чтобы не сделать download/x в downloadx
+    print ("Download: %s - %s" % data).encode('utf-8'), track.get('url')
     filename = ("%s - %s.mp3" % data).replace("/", "")
     filename = "%s/%s" % (group_id, filename)
     if os.path.isfile(filename):
         print "... already here, skip"
         return
-    if not track['url']:
-        print '... no url, skip'
-        return
-    urlretrieve(track['url'], filename)
+    try:
+        urlretrieve(track['url'], filename)
+    except IOError:
+        print '... hmmm, IOError :('
 
 
 def download_all_response(url, group_id):
-    """выкачиваем все аудио из полученного ответа"""
+    """iterate tracks in current response, call download() for each of them"""
     response = json.load(urlopen(url)).get('response')[1:]
     posts = [post for post in response if post.get('attachments')]
     for post in posts:
-        attachments = [a for a in post['attachments'] if a['type'] == 'audio']
+        is_audio = lambda at: at['type'] == 'audio' and at.get('url')
+        attachments = filter(is_audio, post['attachments'])
         attachments = map(simplify, attachments)
         for attach in attachments:
             download(attach, group_id)
 
 
-def main():
-    """проходимся по стене и выкачиваем с неё все трэки"""
-    group_id = get_group_id()
-    wall_get_url = '%s/wall.get?owner_id=-%s' % (API_URL, group_id,)
-    wall_post_count = wall_post_count_get(wall_get_url)
+def __main__(group_id):
+    if len(argv) > 1:
+        group_id = argv[1].strip('/')
     if not os.path.isdir(group_id):
         os.mkdir(group_id)
-    for offset in range(0, wall_post_count, 100):
-        url = "%s&count=100&offset=%d" % (wall_get_url, offset)
+    wall_url = '%s/wall.get?owner_id=-%s' % (API_URL, group_id)
+    post_count = json.load(urlopen(wall_url + '&count=1')).get('response')[0]
+    for offset in range(0, post_count, 100):
+        url = "%s&count=100&offset=%d" % (wall_url, offset)
         download_all_response(url, group_id)
 
 if __name__ == '__main__':
     if '--help' in argv:
         print __doc__
         exit()
-    main()
+    __main__(VK_GROUP_ID)
